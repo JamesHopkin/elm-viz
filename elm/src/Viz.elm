c674397007765d470d8c5da2187594200456e1b8
James Hopkin <james.hopkin@epicgames.com>
2020-06-14 20:24:07 +0100

    Fixed warnings

--- elm/src/Viz.elm
+++ elm/src/Viz.elm
@@ -1,9 +1,8 @@
 port module Viz exposing (main)
 
 import Browser
-import Html exposing (..)
+import Html exposing (div, text)
 
-import Json.Decode as Decode
 import Json.Encode as Encode
 
 port testPort : Encode.Value -> Cmd msg
@@ -12,28 +11,25 @@ type alias Msg = Int
 
 type alias Model =
   { woo: Int
+  , foo: String
   }
 
 
-
-
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = (model, Cmd.none)
+update _ model = (model, Cmd.none)
 
 subscription : Model -> Sub Msg
-subscription model = Sub.none
+subscription _ = Sub.none
 
 type alias Flags = Int --Decode.Value
 
 
 init : Flags -> (Model, Cmd Msg)
-init n = ({ woo = n }, Cmd.none)
+init n = ({ woo = n, foo = "foo" }, Cmd.none)
 
-
---(\k -> \v -> \b -> text k :: b)
 view : Model -> Html.Html Msg
 view model =
-  let s = "hello, elm made me! here's a number: " ++ (String.fromInt model.woo)
+  let s = "hello, elm made me! here's a number: " ++ String.fromInt model.woo
   in div [] [ text s ]
 
 main : Program Flags Model Msg
@@ -44,4 +40,3 @@ main =
     , update = update
     , subscriptions = subscription
     }
-
