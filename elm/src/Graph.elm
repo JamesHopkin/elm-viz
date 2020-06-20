8c724d0737d3967d432cc90375326e12da195eaa
James Hopkin <james.hopkin@epicgames.com>
2020-06-20 17:38:09 +0100

    Dot generated by Elm

--- elm/src/Graph.elm
+++ elm/src/Graph.elm
@@ -1,5 +1,10 @@
-module Graph exposing (Node, Edge)
+module Graph exposing ( Graph(..), toDot,
+  Node(..), NodeProperties, defaultNodeProperties,
+  Edge(..), EdgeProperties, defaultEdgeProperties )
 
+type Graph = Graph (List Node) (List Edge)
+
+-- todo graph properties
 
 type alias NodeProperties =
   { label: String
@@ -49,8 +54,8 @@ b = Node "B"
   | label = "this is B!"
   }
 
-edge : Edge
-edge = Edge a b
+testedge : Edge
+testedge = Edge a b
   { defaultEdgeProperties
   | label = "test"
   }
@@ -61,3 +66,40 @@ type alias Node = { id: String }
 type alias Edge = { from: Node, to: Node }
 edgeToString e = e.from.id ++ " -> " ++ e.to.id
 -}
+
+toDot graph = 
+  case graph of
+    Graph nodes edges ->
+      let
+        renderNode : Node -> String
+        renderNode node = 
+          case node of
+            Node id props ->
+              let
+                suffix = case props.label of 
+                  "" -> ""
+                  _ -> " [label=\"" ++ props.label ++ "\"]"
+              in
+                id ++ suffix ++ ";"
+
+        renderEdge : Edge -> String
+        renderEdge edge = 
+          case edge of
+            Edge (Node fromId _) (Node toId _) _ -> fromId ++ " -> " ++ toId 
+
+        lines = 
+          [ "digraph G {" ] ++
+          (List.map renderNode nodes) ++
+          (List.map renderEdge edges) ++
+          [ "}" ]
+      in
+        String.join "\n" lines
+
+  --"""digraph G {
+  --  node [shape=box];
+  --  From [href="javascript:linkTest('woo!')"];
+
+  --  From -> Elm [style=dashed, arrowhead=none];
+  --  From -> another;
+  --}
+  --"""
