0e797c7d44f57b7c70bab292d90c3f212030deb1
James Hopkin <james.hopkin@epicgames.com>
2020-07-06 08:19:36 +0100

    4 spaces indentation

--- elm/src/Baba/BabaTest.elm
+++ elm/src/Baba/BabaTest.elm
@@ -15,99 +15,99 @@ import Time
 
 -- random from seed
 testGrid = 
-    --LinkedGrid.fromLists emptyCell 4 4
-    --  (stringListToCells
-    --    [ "→PP→" ]
-    --  )
+        --LinkedGrid.fromLists emptyCell 4 4
+        --  (stringListToCells
+        --    [ "→PP→" ]
+        --  )
 
-    --LinkedGrid.fromLists emptyCell 4 4
-    --  [ [ []
-    --    , [ ( 0, '→' ), ( 1, '→' ) ]
-    --    , [ ( 2, 'P') ]
-    --    ]
-    --  ]
+        --LinkedGrid.fromLists emptyCell 4 4
+        --  [ [ []
+        --    , [ ( 0, '→' ), ( 1, '→' ) ]
+        --    , [ ( 2, 'P') ]
+        --    ]
+        --  ]
 
-    Random.step generator seed
-      |> Tuple.first
-      |> makeRandomGrid
+        Random.step generator seed
+            |> Tuple.first
+            |> makeRandomGrid
 
 problemGraphEvo =
-  [ testGrid
-  , testGrid |> doMovesAndPushes
-  , testGrid |> doMovesAndPushes |> doMovesAndPushes
-  , testGrid |> doMovesAndPushes |> doMovesAndPushes |> doMovesAndPushes
-  ]
+    [ testGrid
+    , testGrid |> doMovesAndPushes
+    , testGrid |> doMovesAndPushes |> doMovesAndPushes
+    , testGrid |> doMovesAndPushes |> doMovesAndPushes |> doMovesAndPushes
+    ]
 
 init : (Msg -> msg) -> ( Model, Cmd msg )
 init msg =
-  ( [ testGrid
+    ( [ testGrid
 
 
 
 
-    , movesTestGrid
-    , LinkedGrid.fromLists emptyCell 7 7
-      (stringListToCells
-        [ ""
-        , "···↑·↑"
-        , "·P··↑"
-        , "·P·↑"
-        , "P↑"
-        , "↑"
+        , movesTestGrid
+        , LinkedGrid.fromLists emptyCell 7 7
+            (stringListToCells
+                [ ""
+                , "···↑·↑"
+                , "·P··↑"
+                , "·P·↑"
+                , "P↑"
+                , "↑"
+                ]
+            )
+        , LinkedGrid.fromLists emptyCell 4 4
+            (stringListToCells
+                [ "·↓"
+                , "··←"
+                ]
+            )
         ]
-      )
-    , LinkedGrid.fromLists emptyCell 4 4
-      (stringListToCells
-        [ "·↓"
-        , "··←"
-        ]
-      )
-    ]
-  , Random.generate (RandomGrid >> msg) generator
-  )
+    , Random.generate (RandomGrid >> msg) generator
+    )
 
 
 rulesTestResult =
-    let
-      ruleStrings = List.map ruleDebugString (lookForRules rulesTestGrid)
-    in 
-      String.join "\n" ruleStrings
+        let
+            ruleStrings = List.map ruleDebugString (lookForRules rulesTestGrid)
+        in 
+            String.join "\n" ruleStrings
 
 
 makeRandomGrid : List Char -> Grid
 makeRandomGrid chars =
-  let
-    impl : List Char -> List String -> List String
-    impl innerChars acc = case List.take randomGridSize innerChars of
-      [] -> acc
-      block -> (String.fromList block) :: (impl (List.drop randomGridSize innerChars) acc)
-  in
-    LinkedGrid.fromLists emptyCell randomGridSize randomGridSize (stringListToCells (impl chars []))
+    let
+        impl : List Char -> List String -> List String
+        impl innerChars acc = case List.take randomGridSize innerChars of
+            [] -> acc
+            block -> (String.fromList block) :: (impl (List.drop randomGridSize innerChars) acc)
+    in
+        LinkedGrid.fromLists emptyCell randomGridSize randomGridSize (stringListToCells (impl chars []))
 
 
 rulesTestGrid : Grid
 rulesTestGrid = LinkedGrid.fromLists emptyCell 5 5 (stringListToCells testRows)
 
 movesTestGrid = LinkedGrid.fromLists emptyCell 7 7
-  (stringListToCells
-    [ ""
-    , "···P"
-    , "···↑"
-    , "xP→·←PS"
-    , "···↓"
-    , "···P"
-    ]
-  )
+    (stringListToCells
+        [ ""
+        , "···P"
+        , "···↑"
+        , "xP→·←PS"
+        , "···↓"
+        , "···P"
+        ]
+    )
 
 gridToStr = LinkedGrid.toDebugString cellDebugString
 
 testRows =
-  [ ""
-  , "·a=P·"
-  , "b=c"
-  , "··<"
-  , "··d→"
-  ]
+    [ ""
+    , "·a=P·"
+    , "b=c"
+    , "··<"
+    , "··d→"
+    ]
 
 
 rulesTestGridDebugStr = gridToStr rulesTestGrid
@@ -115,7 +115,7 @@ rulesTestGridDebugStr = gridToStr rulesTestGrid
 
 roll : Random.Generator Int
 roll =
-  Random.int 1 6
+    Random.int 1 6
 
 
 
@@ -124,22 +124,22 @@ type alias Model = List Grid
 
 
 type Msg
-  = Update Time.Posix
-  | RandomGrid (List Char)
+    = Update Time.Posix
+    | RandomGrid (List Char)
 
 seed = Random.initialSeed 4
 generator = 
-  Random.list (randomGridSize * randomGridSize) <| Random.uniform '·' (String.toList "························←↑→↓PPPPSS")
+    Random.list (randomGridSize * randomGridSize) <| Random.uniform '·' (String.toList "························←↑→↓PPPPSS")
 
 
 update : Msg -> Model -> Model 
 update msg model =
-  case msg of
-    Update _
-      -> List.map doMovesAndPushes model
+    case msg of
+        Update _
+            -> List.map doMovesAndPushes model
 
-    RandomGrid chars
-      -> (makeRandomGrid chars) :: model
+        RandomGrid chars
+            -> (makeRandomGrid chars) :: model
 
 subscription : (Msg -> msg) -> Sub msg
 subscription msg = Time.every 300 (Update >> msg)
--- elm/src/Baba/Cell.elm
+++ elm/src/Baba/Cell.elm
@@ -1,7 +1,7 @@
 module Baba.Cell exposing ( Object, Cell, Location, Grid, Axis, emptyCell, moveToCell,
-                          isMove, hasMove, isPush, hasPush, isStop, hasStop, asText, asVerb,
-                          getObjectId, flipDir, moveDir,
-                          cellDebugString, stringListToCells, verbFromOccupant )
+                            isMove, hasMove, isPush, hasPush, isStop, hasStop, asText, asVerb,
+                            getObjectId, flipDir, moveDir,
+                            cellDebugString, stringListToCells, verbFromOccupant )
 
 import List.Extra
 
@@ -26,62 +26,62 @@ getObjectId ( id, _ ) = id
 
 
 addToCellAt offset object axis =
-  let
-    newContents = object :: (LinkedGrid.axisGetAt offset axis)
-  in
-    LinkedGrid.axisSet newContents axis
+    let
+        newContents = object :: (LinkedGrid.axisGetAt offset axis)
+    in
+        LinkedGrid.axisSet newContents axis
 addToCell = addToCellAt 0
 
 
 moveToCell : Int -> Int -> Int -> Axis -> Maybe Axis
 moveToCell id from to axis =
-  let
-    fromContent = LinkedGrid.axisGetAt from axis
-  
-  in
-    case List.Extra.find (Tuple.first >> ((==) id)) fromContent of
-        Just obj -> 
-          let
-            newFromContent = List.filter (Tuple.first >> ((/=) id)) fromContent
-            newToContent = obj :: LinkedGrid.axisGetAt to axis
-          in
-            Just
-              ( axis
-  -- could optimise to not replace grid twice
-                  |> LinkedGrid.axisSetAt from newFromContent
-                  |> LinkedGrid.axisSetAt to newToContent
-              )
+    let
+        fromContent = LinkedGrid.axisGetAt from axis
+    
+    in
+        case List.Extra.find (Tuple.first >> ((==) id)) fromContent of
+                Just obj -> 
+                    let
+                        newFromContent = List.filter (Tuple.first >> ((/=) id)) fromContent
+                        newToContent = obj :: LinkedGrid.axisGetAt to axis
+                    in
+                        Just
+                            ( axis
+    -- could optimise to not replace grid twice
+                                    |> LinkedGrid.axisSetAt from newFromContent
+                                    |> LinkedGrid.axisSetAt to newToContent
+                            )
 
-        _ -> Nothing
+                _ -> Nothing
 
 
 verbFromOccupant c = case c of
-  'P' -> Push
-  'M' -> Move
-  'D' -> Defeat
-  'W' -> Win
-  'O' -> Open
-  'C' -> Closed
-  'F' -> Float_
-  _ -> You
+    'P' -> Push
+    'M' -> Move
+    'D' -> Defeat
+    'W' -> Win
+    'O' -> Open
+    'C' -> Closed
+    'F' -> Float_
+    _ -> You
 
 moveDir ( _, c ) = case c of
-  '←' -> Just Left
-  '↑' -> Just Up
-  '→' -> Just Right
-  '↓' -> Just Down
-  _ -> Nothing
+    '←' -> Just Left
+    '↑' -> Just Up
+    '→' -> Just Right
+    '↓' -> Just Down
+    _ -> Nothing
 
 flipDir ( id, c ) = case c of
-  '←' -> Just ( id, '→' )
-  '↑' -> Just ( id, '↓' )
-  '→' -> Just ( id, '←' )
-  '↓' -> Just ( id, '↑' )
-  _ -> Nothing
+    '←' -> Just ( id, '→' )
+    '↑' -> Just ( id, '↓' )
+    '→' -> Just ( id, '←' )
+    '↓' -> Just ( id, '↑' )
+    _ -> Nothing
 
 isMove obj = case moveDir obj of
-  Nothing -> False
-  _ -> True
+    Nothing -> False
+    _ -> True
 
 hasMove = List.any isMove
 
@@ -92,24 +92,24 @@ isStop ( _, c ) = c == 'S'
 hasStop cell = List.any isStop cell
 
 asText cell =
-  let
-    filtered = cell
-      |> List.map Tuple.second
-      |> List.filter Char.isAlpha
-  in
-    case filtered of
-      first :: rest -> Just first
-      _ -> Nothing
+    let
+        filtered = cell
+            |> List.map Tuple.second
+            |> List.filter Char.isAlpha
+    in
+        case filtered of
+            first :: rest -> Just first
+            _ -> Nothing
 
 asVerb cell =
-  let
-    filtered = cell
-      |> List.map Tuple.second
-      |> List.filter Char.isUpper
-  in
-    case filtered of
-      first :: rest -> Just first
-      _ -> Nothing
+    let
+        filtered = cell
+            |> List.map Tuple.second
+            |> List.filter Char.isUpper
+    in
+        case filtered of
+            first :: rest -> Just first
+            _ -> Nothing
 
 
 
@@ -119,42 +119,42 @@ showAllContents = False
 
 -- char of arbitrary item at the moment
 cellDebugString cell =
-  if showIds then
-    List.map (\( id, c ) -> String.fromInt id) cell
-     |> String.join ""
-  else
-    let
-      toString c = String.fromChar (if c == '·' then '!' else c)
-      chars = List.map Tuple.second cell
-    in
-      if showAllContents then
-        case chars of
-          [] -> "·"
-          _ -> String.fromList chars
+    if showIds then
+        List.map (\( id, c ) -> String.fromInt id) cell
+         |> String.join ""
+    else
+        let
+            toString c = String.fromChar (if c == '·' then '!' else c)
+            chars = List.map Tuple.second cell
+        in
+            if showAllContents then
+                case chars of
+                    [] -> "·"
+                    _ -> String.fromList chars
 
-      else
-        case chars of
-          first :: second :: _ -> (toString first) ++ (toString second)
-          first :: _           -> (toString first) ++ " "
-          _ -> "· "
+            else
+                case chars of
+                    first :: second :: _ -> (toString first) ++ (toString second)
+                    first :: _           -> (toString first) ++ " "
+                    _ -> "· "
 
 
 stringListToCells : List String -> List (List Cell)
 stringListToCells rows =
-  let
-    makeCell : Char -> ( Int, List Cell ) -> ( Int, List Cell )
-    makeCell c ( index, outRow ) =
-      if c == '·' then ( index, [] :: outRow )
-      else ( index + 1, [( index, c )] :: outRow )
+    let
+        makeCell : Char -> ( Int, List Cell ) -> ( Int, List Cell )
+        makeCell c ( index, outRow ) =
+            if c == '·' then ( index, [] :: outRow )
+            else ( index + 1, [( index, c )] :: outRow )
 
-    makeRow : String -> ( Int, List (List Cell) ) -> ( Int, List (List Cell) )
-    makeRow s ( initialIndex, outRows ) =
-        s
-          |> String.toList
-          |> List.foldr makeCell ( initialIndex, [] )
-          |> \( index, cells ) -> ( index, cells :: outRows)
+        makeRow : String -> ( Int, List (List Cell) ) -> ( Int, List (List Cell) )
+        makeRow s ( initialIndex, outRows ) =
+                s
+                    |> String.toList
+                    |> List.foldr makeCell ( initialIndex, [] )
+                    |> \( index, cells ) -> ( index, cells :: outRows)
 
-  in
-    --List.foldr (String.toList >> (List.foldr makeCell) >> Tuple.second) ( 0, [] ) rows
-    List.foldr makeRow ( 0, [] ) rows |> Tuple.second
+    in
+        --List.foldr (String.toList >> (List.foldr makeCell) >> Tuple.second) ( 0, [] ) rows
+        List.foldr makeRow ( 0, [] ) rows |> Tuple.second
 
